bf98f1e00477521d59fcbe1b80fa753d1d956552
tlb <tlb@ubuntu.myguest.virtualbox.org>
2023-07-31 12:24:00 +0330

    adjust cell's style and grid size

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
 const container = document.getElementById('container');
 function makeGrid(rows, cols){
-    container.style.setProperty('grid-cols', cols);
-    container.style.setProperty('grid-rows',rows);
+    container.style.setProperty('--grid-rows',rows);
+    container.style.setProperty('--grid-cols',cols);
     for(let i = 0 ; i<(rows*cols) ; i++){
         let cell = document.createElement('div');
-        container.appendChild(cell).className = 'grid-item'
+        container.appendChild(cell).className = 'grid-item';
     }
 }
+
+makeGrid(16,16);
