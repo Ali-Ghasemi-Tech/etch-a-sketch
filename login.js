f7978c671084f67df31937d55a720fa6ca00aec8
tlb <tlb@ubuntu.myguest.virtualbox.org>
2023-08-03 12:12:58 +0330

    add draw with mouse hover

--- login.js
+++ login.js
@@ -4,8 +4,15 @@ function makeGrid(rows, cols){
     container.style.setProperty('--grid-cols',cols);
     for(let i = 0 ; i<(rows*cols) ; i++){
         let cell = document.createElement('div');
-        container.appendChild(cell).className = 'grid-item';
+        container.appendChild(cell).className = `grid-item number-${i+1}`;
     }
 }
 
 makeGrid(16,16);
+
+for(let i=0; i < 16*16; i++){
+    let cell = document.querySelector(`.number-${i+1}`);
+    cell.addEventListener('mouseenter', () => {
+    cell.style.background = "gray";
+    })
+}
